5641d99aa629663c534bdec070b5c0d8abde2a28
pushkardubey123 <pushkardubey1407@gmail.com>
2025-06-06 12:37:36 +0530

    links deleted

--- Dbconnect/dbConfig.js
+++ Dbconnect/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const dbConnect=()=>{
     const URI=process.env.MONGO_URL
-    const con =mongoose.connect(URI)
+    const con =mongoose.connect(`${URI}`)
     if (con) {
         console.log("Database connected !")
     }
