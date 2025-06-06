ef8cdddfa0d40e5addc048ef8c70966c749da50a
pushkardubey123 <pushkardubey1407@gmail.com>
2025-06-06 14:01:54 +0530

    mongourl update

--- Dbconnect/dbConfig.js
+++ Dbconnect/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const dbConnect=()=>{
     const URI=process.env.MONGO_URL
-    const con =mongoose.connect(`${URI}`)
+    const con =mongoose.connect("mongodb://localhost:27017/HR_Hareetech")
     if (con) {
         console.log("Database connected !")
     }
