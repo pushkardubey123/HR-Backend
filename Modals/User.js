7324d4e315940a97f236e1243d4e37c18bdccdf9
pushkardubey123 <pushkardubey1407@gmail.com>
2025-06-10 13:04:05 +0530

    first commit

--- Modals/User.js
+++ Modals/User.js
@@ -1,27 +1,36 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  role: { type: String, enum: ["admin", "employee"], required: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: String,
-  passwordHash: { type: String, required: true },
-  gender: String,
-  dob: Date,
-  address: String,
-  departmentId: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
-  designationId: { type: mongoose.Schema.Types.ObjectId, ref: "Designation" },
-  shiftId: { type: mongoose.Schema.Types.ObjectId, ref: "Shift" },
-  doj: Date,
-  status: { type: String, enum: ["active", "inactive", "resigned"], default: "active" },
-  documents: [String],
-  profilePic: String,
-  emergencyContact: {
-    name: String,
+const UserSchema = new mongoose.Schema(
+  {
+    role: { type: String, enum: ["admin", "employee"], required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     phone: String,
-    relation: String,
-  }
-}, { timestamps: true });
+    passwordHash: { type: String, required: true },
+    gender: String,
+    dob: Date,
+    address: String,
+    departmentId: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
+    designationId: { type: mongoose.Schema.Types.ObjectId, ref: "Designation" },
+    shiftId: { type: mongoose.Schema.Types.ObjectId, ref: "Shift" },
+    doj: Date,
+    status: {
+      type: String,
+      enum: ["active", "inactive", "resigned"],
+      default: "active",
+    },
+    documents: [String],
+    profilePic: String,
+    emergencyContact: {
+      name: String,
+      phone: String,
+      relation: String,
+      otp: String,
+      otpExpires: Date,
+    },
+  },
+  { timestamps: true }
+);
 
 const userTbl = mongoose.model("User", UserSchema);
 module.exports = userTbl;
---
+++ utils/sendOtp.js
@@ -0,0 +1,20 @@
+ const nodemailer = require("nodemailer");
+
+const sendOTP = async (email, otp) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER, 
+      pass: process.env.EMAIL_PASS, 
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"HRMS App" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: "Password reset OTP",
+    html: `<h3>Your OTP is: <b>${otp}</b></h3><p>Iys is expire in 10 minutes/p>`,
+  });
+};
+
+module.exports = sendOTP;
