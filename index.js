71b117213ac525d5b960e50a2e698193ba3e7615
pushkardubey123 <pushkardubey1407@gmail.com>
2025-08-05 16:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://hareetech.netlify.app'],
+  origin: ['http://localhost:5173', 'https://hareetech.netlify.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
