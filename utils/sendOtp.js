437bfa83263390b19d0d14f9cf00573d425a1321
pushkardubey123 <pushkardubey1407@gmail.com>
2025-10-10 14:08:07 +0530

    first commit

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -4,11 +4,11 @@ const sendOTP = async (email, otp) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465, // use 465 for SSL
+      port: 465, 
       secure: true,
       auth: {
-        user: process.env.EMAIL_USER, // your Gmail address
-        pass: process.env.EMAIL_PASS, // your Gmail app password (16-digit)
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS, 
       },
     });
 
@@ -19,9 +19,9 @@ const sendOTP = async (email, otp) => {
       html: `<h3>Your OTP is: <b>${otp}</b></h3><p>This OTP will expire in 10 minutes.</p>`,
     });
 
-    console.log("OTP Email sent successfully ✅");
+    console.log("OTP Email sent successfully");
   } catch (err) {
-    console.error("Error sending OTP email ❌", err);
+    console.error("Error sending OTP email", err);
     throw new Error("Failed to send OTP email");
   }
 };
